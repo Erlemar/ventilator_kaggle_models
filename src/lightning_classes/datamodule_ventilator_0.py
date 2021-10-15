9048f3c674ad53b7be5305d9e7900427c4fb4fa8
Andrey <andlukyane@gmail.com>
2021-10-15 16:29:23 +0300

    more features

--- src/lightning_classes/datamodule_ventilator_0.py
+++ src/lightning_classes/datamodule_ventilator_0.py
@@ -346,7 +346,7 @@ class VentilatorDataModule(pl.LightningDataModule):
         if 'pressure' in data.columns:
             data.drop('pressure', axis=1, inplace=True)
 
-        return data
+        return data.fillna(0)
 
     def make_features4(self, data):
         data['area'] = data['time_step'] * data['u_in']
@@ -538,87 +538,87 @@ class VentilatorDataModule(pl.LightningDataModule):
         if 'pressure' in data.columns:
             data.drop('pressure', axis=1, inplace=True)
 
-        return data
+        return data.fillna(0)
 
     def setup(self, stage=None):
-        if self.cfg.dataing.debug:
-            data = pd.read_csv(os.path.join(self.cfg.datamodule.path, 'data.csv'), nrows=196000)
+        if self.cfg.training.debug:
+            train = pd.read_csv(os.path.join(self.cfg.datamodule.path, 'train.csv'), nrows=196000)
             test = pd.read_csv(os.path.join(self.cfg.datamodule.path, 'test.csv'), nrows=80000)
         else:
-            data = pd.read_csv(os.path.join(self.cfg.datamodule.path, 'data.csv'))
+            train = pd.read_csv(os.path.join(self.cfg.datamodule.path, 'train.csv'))
             test = pd.read_csv(os.path.join(self.cfg.datamodule.path, 'test.csv'))
 
-        targets = data[['pressure']].to_numpy().reshape(-1, 80)
+        targets = train[['pressure']].to_numpy().reshape(-1, 80)
         test_targets = np.zeros(len(test)).reshape(-1, 80)
 
         print('Making features')
         if self.cfg.datamodule.make_features_style == 0:
-            data = self.make_features(data)
+            train = self.make_features(train)
             test = self.make_features(test)
         elif self.cfg.datamodule.make_features_style == 1:
-            data = self.make_features1(data)
+            train = self.make_features1(train)
             test = self.make_features1(test)
         elif self.cfg.datamodule.make_features_style == 2:
-            data = self.make_features2(data)
+            train = self.make_features2(train)
             test = self.make_features2(test)
         elif self.cfg.datamodule.make_features_style == 3:
-            data = self.make_features3(data)
+            train = self.make_features3(train)
             test = self.make_features3(test)
         elif self.cfg.datamodule.make_features_style == 4:
-            data = self.make_features4(data)
+            train = self.make_features4(train)
             test = self.make_features4(test)
 
-        data_u_out = data[['u_out']].to_numpy().reshape(-1, 80)
+        train_u_out = train[['u_out']].to_numpy().reshape(-1, 80)
         test_u_out = test[['u_out']].to_numpy().reshape(-1, 80)
 
         if self.cfg.datamodule.normalize:
             RS = RobustScaler()
-            data = RS.fit_transform(data)
+            train = RS.fit_transform(train)
             test = RS.transform(test)
         else:
-            data = data.values
+            train = train.values
             test = test.values
 
-        data = data.reshape(-1, 80, data.shape[-1])
-        test = test.reshape(-1, 80, data.shape[-1])
-        gkf = KFold(n_splits=self.cfg.datamodule.n_folds, shuffle=True, random_state=self.cfg.dataing.seed)
+        train = train.reshape(-1, 80, train.shape[-1])
+        test = test.reshape(-1, 80, train.shape[-1])
+        gkf = KFold(n_splits=self.cfg.datamodule.n_folds, shuffle=True, random_state=self.cfg.training.seed)
         # if self.cfg.datamodule.split == 'GroupKFold':
         #     gkf = GroupKFold(n_splits=self.cfg.datamodule.n_folds)
         # elif self.cfg.datamodule.split == 'GroupShuffleSplit':
-        #     gkf = GroupShuffleSplit(n_splits=self.cfg.datamodule.n_folds, random_state=self.cfg.dataing.seed)
+        #     gkf = GroupShuffleSplit(n_splits=self.cfg.datamodule.n_folds, random_state=self.cfg.training.seed)
 
-        splits = list(gkf.split(X=data, y=targets))
-        data_idx, valid_idx = splits[self.cfg.datamodule.fold_n]
+        splits = list(gkf.split(X=train, y=targets))
+        train_idx, valid_idx = splits[self.cfg.datamodule.fold_n]
 
-        data_data = data[data_idx].copy()
-        valid_data = data[valid_idx].copy()
+        train_data = train[train_idx].copy()
+        valid_data = train[valid_idx].copy()
 
-        data_u_out_ = data_u_out[data_idx].copy()
-        valid_u_out_ = data_u_out[valid_idx].copy()
+        train_u_out_ = train_u_out[train_idx].copy()
+        valid_u_out_ = train_u_out[valid_idx].copy()
 
-        data_targets = targets[data_idx].copy()
+        train_targets = targets[train_idx].copy()
         valid_targets = targets[valid_idx].copy()
 
-        del data
+        del train
         gc.collect()
 
-        # data dataset
+        # train dataset
         dataset_class = load_obj(self.cfg.datamodule.class_name)
 
-        self.data_dataset = dataset_class(data_data, mode='data', u_out=data_u_out_, pressure=data_targets)
+        self.train_dataset = dataset_class(train_data, mode='train', u_out=train_u_out_, pressure=train_targets)
         self.valid_dataset = dataset_class(valid_data, mode='valid', u_out=valid_u_out_, pressure=valid_targets)
         self.test_dataset = dataset_class(test, mode='test', u_out=test_u_out, pressure=test_targets)
 
-    def data_dataloader(self):
-        data_loader = DataLoader(
-            self.data_dataset,
+    def train_dataloader(self):
+        train_loader = DataLoader(
+            self.train_dataset,
             batch_size=self.cfg.datamodule.batch_size,
             num_workers=self.cfg.datamodule.num_workers,
             pin_memory=self.cfg.datamodule.pin_memory,
             shuffle=True,
             drop_last=True
         )
-        return data_loader
+        return train_loader
 
     def val_dataloader(self):
         valid_loader = DataLoader(
